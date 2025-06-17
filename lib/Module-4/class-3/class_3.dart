ae15201d69fc448776c6358243ae7e5774991a75
Mynul Alam <mynulalam27@gmail.com>
2025-06-17 23:27:19 +0600

    commit

--- lib/Module-4/class-3/class_3.dart
+++ lib/Module-4/class-3/class_3.dart
@@ -1,1 +1,12 @@
+import 'package:dart_advance/Module-4/class-3/resturant.dart';
 
+main() {
+  Restaurant BFC = Restaurant();
+  BFC.order('ckn fry');
+
+  ///// gov////
+
+  BFC.setID = 6565;
+
+  print(BFC.RestaurantID);
+}
